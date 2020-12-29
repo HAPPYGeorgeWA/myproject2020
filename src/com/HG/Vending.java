18ea75b9df3e106494377fa8f4d51c26b7ce3108
User <User@ES301N10>
2020-12-29 16:35:10 +0800

    add read data.txt

--- src/com/HG/Vending.java
+++ src/com/HG/Vending.java
@@ -1,10 +1,50 @@
 package com.HG;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Vending {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try{
+			FileInputStream fis= new FileInputStream("data.txt");
+			InputStreamReader si= new InputStreamReader(fis);
+			BufferedReader bi = new BufferedReader(si);
+			String s = bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+			System.out.println(s);
+			s=bi.readLine();
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch (IOException e) {
+			System.out.println("Error reading");
+		}
+		
+		
+		
+		
+		
+		
+		
+		/*try {
+			FileReader reader =new FileReader("data1.txt");
+			System.out.println("file opened");
+			
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		System.out.println("End");*/
 	}
 
 }
